cb4a72b62b44bcf579709d7598e2d8d3f95938ae
usidedown <usidedown@gmail.com>
2017-12-17 00:36:16 +0200

    Implemented insert

--- src/bst.hpp
+++ src/bst.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <deque>
+#include <tuple> // for pair
+#include <limits> // for max limit
 
 template <typename T>
 struct node
@@ -18,10 +20,12 @@ class binary_search_tree
 	using Container = std::deque<node<T>>; // TODO use template magicke to allow to change this to vector
 	using size_type = typename Container::size_type;
 
+	static const auto NULL_VALUE = std::numeric_limits<size_type>::max();
+
 public:
 	// TODO return iterator
 	// TODO T&& version
-	size_type insert(const T& value);
+	std::pair<size_type,bool> insert(const T& value);
 
 	// TODO return iterator
 	// TODO T&& version
@@ -40,13 +44,81 @@ public:
 		return _storage.size();
 	}
 
+	bool empty() const
+	{
+		return _storage.empty();
+	}
+
 	void reserve(size_type capacity)
 	{
 		_storage.reserve(capacity);
 	}
 
 private:
+
+
+private:
 	Container _storage;
 	size_type root;
 };
 
+template <typename T>
+std::pair<typename binary_search_tree<T>::size_type, bool> binary_search_tree<T>::insert(const T& value)
+{
+	if (empty())
+	{
+		_storage.push_back(node<T>{ NULL_VALUE, NULL_VALUE, NULL_VALUE, value});
+
+		root = 0;
+		return { 0, true };
+	}
+
+	// gotta find position
+	auto cur = root;
+	auto prev = cur;
+	bool is_left_child = false;
+	while (true)
+	{
+		const auto& cur_node = _storage[cur];
+		const auto& cur_value = cur_node.value;
+		const auto& left = cur_node.left;
+		const auto& right = cur_node.right;
+
+		prev = cur;
+
+		if (value < cur_value)
+		{
+			cur = left;
+			is_left_child = true;
+		}
+		else if (value > cur_value)
+		{
+			cur = right;
+			is_left_child = false;
+
+		}
+		else
+		{
+			return { cur, false };
+		}
+
+		if (cur == NULL_VALUE)
+		{
+			break;
+		}
+	}
+
+	_storage.push_back(node<T>{ prev, NULL_VALUE, NULL_VALUE, value});
+	const auto pos = size() - 1;
+
+	if (is_left_child)
+	{
+		_storage[prev].left = pos;
+	}
+	else
+	{
+		_storage[prev].right = pos;
+	}
+
+	return { pos, true };
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,10 +1,47 @@
+#pragma push_macro("_SILENCE_CXX17_UNCAUGHT_EXCEPTION_DEPRECATION_WARNING")
+#define _SILENCE_CXX17_UNCAUGHT_EXCEPTION_DEPRECATION_WARNING
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#undef _SILENCE_CXX17_UNCAUGHT_EXCEPTION_DEPRECATION_WARNING
+#pragma pop_macro("_SILENCE_CXX17_UNCAUGHT_EXCEPTION_DEPRECATION_WARNING")
 
 #include "bst.hpp"
 
-TEST_CASE("Can create empty bst", "[bst]") {
+TEST_CASE("Can create empty bst", "[ctor]") {
 	binary_search_tree<int> bstint;
 
 	REQUIRE(bstint.size() == 0);
+}
+
+TEST_CASE("inserting to bst works", "[insert]") {
+	binary_search_tree<int> bstint;
+	
+	const auto [index, inserted] = bstint.insert(5);
+	REQUIRE(index == 0);
+	REQUIRE(inserted == true);
+	REQUIRE(bstint.size() == 1);
+
+	SECTION("Inserting before works")
+	{
+		const auto[index, inserted] = bstint.insert(4);
+		REQUIRE(index == 1); // I don't expect it to move the root
+		REQUIRE(inserted == true);
+		REQUIRE(bstint.size() == 2);
+	}
+
+	SECTION("Inserting after works")
+	{
+		const auto[index, inserted] = bstint.insert(6);
+		REQUIRE(index == 1); // I don't expect it to move the root
+		REQUIRE(inserted == true);
+		REQUIRE(bstint.size() == 2);
+	}
+
+	SECTION("Inserting already existing is equivalent to finding")
+	{
+		const auto[index, inserted] = bstint.insert(5);
+		REQUIRE(index == 0);
+		REQUIRE(inserted == false);
+		REQUIRE(bstint.size() == 1);
+	}
 }
